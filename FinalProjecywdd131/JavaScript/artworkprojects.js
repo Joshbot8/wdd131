ea60100719b46f17e8c1dc0044705cdb6e3f5540
Joshbot8 <hoppingamimations105@gmail.com>
2026-07-18 10:10:16 -0700

    New Final Upload

--- FinalProjecywdd131/JavaScript/artworkprojects.js
+++ FinalProjecywdd131/JavaScript/artworkprojects.js
@@ -1,6 +1,6 @@
 ﻿artworkprojects = [
     {
-        tags: ['FN@F', 'Custom Oc', 'Animatronic'],
+        tags: ['Five Nights at Freddys', 'Custom Oc', 'Animatronic'],
         description: 'A Fnaf character of my own creation, Most commonly known on the VRC platform',
         image: 'FinalProjectImage/image-195.webp',
         name: 'Glamrock DJ MusicMan',
@@ -10,9 +10,9 @@
     },
     {
         tags: ['StarWars', 'Custom Oc', 'Droid'],
-        description: 'ExtraInformation',
+        description: 'A Droid Based off of a C1 Seriese Astromech from the Starwars Franchise',
         image: 'FinalProjectImage/image-9(1).webp',
-        name: 'Droid',
+        name: 'C1-0j1',
         prepTime: '5 days',
         rating: 0,
         votes: 0
@@ -28,5 +28,35 @@
         votes: 0
     }
     ]
+unfinishedartworkprojects = [
+    {
+        tags: ['Five Nights at Freddys', 'Custom Oc', 'Animatronic'],
+        description: 'A Fnaf character of my own creation, Most commonly known on the VRC platform',
+        image: 'FinalProjectImage/image-195.webp',
+        name: 'Glamrock DJ MusicMan',
+        prepTime: '2 days',
+        rating: 0,
+        votes: 0
+    },
+    {
+        tags: ['StarWars', 'Custom Oc', 'Droid'],
+        description: 'A Droid Based off of a C1 Seriese Astromech from the Starwars Franchise',
+        image: 'FinalProjectImage/image-9(1).webp',
+        name: 'C1-0j1',
+        prepTime: '5 days',
+        rating: 0,
+        votes: 0
+    },
+    {
+        tags: ["Transformers", "Dinobot", "Grimlock"],
+        description: "3D model of Grimlock in his dinosaur form.",
+        image: "./FinalProjectImage/Neon.webp",
+        model: "./models/Grimlock.glb",
+        name: "Grimlock",
+        timeSpent: "2 weeks",
+        rating: 0,
+        votes: 0
+    }
+]
     
     
---
+++ FinalProjecywdd131/JavaScript/unfinishedartworkproject.js
@@ -0,0 +1,21 @@
+﻿artworkprojects = [
+    {
+        tags: ['Five Nights at Freddys', 'Custom Oc', 'Animatronic'],
+        description: 'Test',
+        image: 'FinalProjectImage/image-173.png',
+        name: 'Glamrock DJ MusicMan WIP',
+        prepTime: '2 days',
+        rating: 0,
+        votes: 0
+    },
+    {
+        tags: ["Bionicle", "Insect", "Elemental Creature"],
+        description: "3D model of a jungle creature from BIONICLE",
+        image: "./FinalProjectImage/Neon.webp",
+        model: "./models/JungleCreature.glb",
+        name: "Unfinished Jungle Creature",
+        timeSpent: "2 weeks",
+        rating: 0,
+        votes: 0
+    }
+]
